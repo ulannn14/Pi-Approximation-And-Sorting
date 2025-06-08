1684865e5a760b833258b02271028ca81f2c45ba
ulannn14 <rlsrainmackenlhy142005@gmail.com>
2025-06-09 00:53:17 +0800

    added computation for three sorts

--- src/SortingAlgorithms.java
+++ src/SortingAlgorithms.java
@@ -88,6 +88,8 @@ public class SortingAlgorithms {
 
     public static void bubbleSortDistanceFromSorted(int numbers[], int sorted[], PrintWriter writer, int N) {
         long comparisons = 0;
+        int totalComparisons = N * (N - 1) / 2;
+        
         for (int i = N - 1; i >= 1; i--) {
             for (int j = 0; j < i; j++) {
                 comparisons++;
@@ -95,12 +97,14 @@ public class SortingAlgorithms {
                     swap(numbers, j, j + 1);
                 }
 
-                if (comparisons % 100000 == 0 ) {
+                if (comparisons % 10000000 == 0) {
                     System.out.println("Comparisons: " + comparisons + " Distance from sorted: " + getDistanceFromSorted(numbers, sorted));
+                    writer.println(comparisons + " " + getDistanceFromSorted(numbers, sorted));
                 }
             }
         }
         System.out.println("Comparisons: " + comparisons + " Distance from sorted: " + getDistanceFromSorted(numbers, sorted));
+        writer.println(comparisons + " " + getDistanceFromSorted(numbers, sorted));
     }
 
 
@@ -117,6 +121,28 @@ public class SortingAlgorithms {
         }
     }
 
+
+    public static void selectionSortDistanceFromSorted(int numbers[], int sorted[], PrintWriter writer, int N) {
+        long comparisons = 0; // <-- use long instead of int
+        for (int i = 0; i <= N-1; i++) {
+            int minIndex = i;
+            for (int j = i + 1; j < N; j++) {
+                if (numbers[j] < numbers[minIndex]) {
+                    minIndex = j;
+                }
+                comparisons++;
+                if (comparisons % 10000000 == 0) {
+                    System.out.println("Comparisons: " + comparisons + " Distance from sorted: " + getDistanceFromSorted(numbers, sorted));
+                    writer.println(comparisons + " " + getDistanceFromSorted(numbers, sorted));
+                }
+            }
+            swap(numbers, i, minIndex);
+        }
+        System.out.println("Comparisons: " + comparisons + " Distance from sorted: " + getDistanceFromSorted(numbers, sorted));
+        writer.println(comparisons + " " + getDistanceFromSorted(numbers, sorted));
+    }
+
+
     public static void insertionSort(int numbers[], PrintWriter writer, int N) {
         int i;
         for (int j = 1; j < N; j++) {
@@ -127,6 +153,28 @@ public class SortingAlgorithms {
             numbers[i+1] = key;
         }
     }
+
+        public static void insertionSortDistanceFromSorted(int numbers[], int sorted[], PrintWriter writer, int N) {
+        int i;
+        long comparisons = 0; // <-- use long instead of int
+        for (int j = 1; j < N; j++) {
+            int key = numbers[j];
+            for (i = j-1; i >= 0 && numbers[i] > key; i--) {
+                numbers[i + 1] = numbers[i];
+                comparisons++;
+                if (comparisons % 10000000 == 0) {
+                    System.out.println("Comparisons: " + comparisons + " Distance from sorted: " + getDistanceFromSorted(numbers, sorted));
+                    writer.println(comparisons + " " + getDistanceFromSorted(numbers, sorted));
+                }
+            }
+            numbers[i+1] = key;
+        }
+        System.out.println("Comparisons: " + comparisons + " Distance from sorted: " + getDistanceFromSorted(numbers, sorted));
+        writer.println(comparisons + " " + getDistanceFromSorted(numbers, sorted));
+    }
+
+
+
 
     public static void quickSort(int[] arr, PrintWriter writer, int low, int high) {
         if (low < high) {
@@ -188,8 +236,9 @@ public class SortingAlgorithms {
         int N = 0;
         String filename = "descending.csv";
         try {
+            System.out.println("Starting the sort. \n\n");
             PrintWriter writer = new PrintWriter("output.txt");
-
+            writer.println("File: " + filename);
             ArrayList<Integer> tempList = populateArrayFromFile(filename);
             N = tempList.size();
 
@@ -198,19 +247,33 @@ public class SortingAlgorithms {
             numbers = tempList.stream().mapToInt(i->i).toArray();
             int[] sorted = numbers.clone();
             Arrays.sort(sorted);
+            /* 
             for (int i = 0; i < N; i++) {
-                System.out.print(numbers[i] + " ");
+                System.out.print(numbers[i] + " " );
                 writer.print(numbers[i] + " ");
             }
+            */
 
             // sortings
-            //selectionSort(numbers, writer, N);  
-            //bubbleSortInversion(numbers, writer, N);
-            bubbleSortDistanceFromSorted(numbers, sorted, writer, N); 
-            // insertionSort(numbers, writer, N); 
-            // quickSort(numbers, writer, 0, N - 1); 
-            // mergeSort(numbers, writer, 0, N - 1); 
 
+
+            // writer.println("Sort: Bubble Sort Distance from Sorted" + "\n");
+            //bubbleSortDistanceFromSorted(numbers, sorted, writer, N); 
+            
+            //writer.println("Sort: Selection Sort Distance from Sorted" + "\n");
+            //selectionSortDistanceFromSorted(numbers, sorted, writer, N);
+            
+            writer.println("Sort: Insertion Sort Distance from Sorted" + "\n");
+            insertionSortDistanceFromSorted(numbers, sorted, writer, N);
+
+            
+
+            
+            //bubbleSortInversion(numbers, writer, N);            
+            // quickSort(numbers, writer, 0, N - 1); 
+            // mergeSort(numbers, writer, 0, N - 1);
+            // shell sort 
+            writer.close(); 
         } catch (FileNotFoundException e) {
             // TODO Auto-generated catch block
             e.printStackTrace();
